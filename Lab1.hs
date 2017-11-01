0c567146d1e519856783669293a17a4febb5ba3c
Baloo <karlmattsson96@gmail.com>
2017-11-01 16:38:06 +0100

    Saker typ testas och funkar med quickCheck.

--- Lab1.hs
+++ Lab1.hs
@@ -10,14 +10,15 @@ power n k = n * power n (k-1)
 
 -- Part 2
 power1 :: Integer -> Integer -> Integer
+power1 n k | k == 0 = 1
 power1 n k | k < 0 = error "power: negative argument"
 power1 n k | k > 0 = product (replicate (fromInteger k) n)
 
 -- Part 3
 power2 :: Integer -> Integer -> Integer
 power2 n k | k < 0 = error "power: negative argument"
-power2 n 0 = 1
-power2 n k = if even k then power2 (n*n) (div k 2) else n * (power2 (n) (k-1))
+power2 n k | k == 0 = 1
+power2 n k = if even k then power2 (n*n) (div k 2) else n * (power2 n (k-1))
 
 
 -- Part 4
@@ -28,5 +29,7 @@ power2 n k = if even k then power2 (n*n) (div k 2) else n * (power2 (n) (k-1))
 -- Finally test for a few positive numbers and compare the results between the three functions to assert that the answer is correct.
 -- B
 
-prop_powers n k = (power n k) == (power1 n k) && (power n k) == (power2 n k)
+prop_powers :: Integer -> Integer -> Bool
+prop_powers n k = (n^absK == power1 n absK) && (n^absK == power2 n absK)
+  where absK = abs k
 -- C
