041c08db6572959e824ebdfc898eeacb82e2dfbd
Baloo <karlmattsson96@gmail.com>
2017-11-06 16:07:24 +0100

    fixed valueRank

--- blackjack/BlackJack.hs
+++ blackjack/BlackJack.hs
@@ -12,9 +12,13 @@ empty :: Hand
 empty = Empty
 
 valueRank :: Rank -> Integer
-
+valueRank Jack = 10
+valueRank Queen = 10
+valueRank King = 10
+valueRank Ace = 11
+valueRank Numeric i = i
 
 valueCard :: Card -> Integer
 
+
 value :: Hand -> Integer
-value Hand
