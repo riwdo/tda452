a93bf5d760ff559bd4ab0fc12973652d560ed594
Baloo <karlmattsson96@gmail.com>
2017-11-07 13:43:17 +0100

    fixed comments

--- blackjack/BlackJack.hs
+++ blackjack/BlackJack.hs
@@ -5,15 +5,17 @@ import RunGame
 import Test.QuickCheck
 
 
--- Implement functions for "empty", "value", "gameOver", and "winner"
+-- arbitrary hands with cards
 hand1 = Add (Card Jack Hearts) empty
 hand2 = Add (Card (Numeric 2) Hearts) (Add (Card Jack Spades) Empty)
 hand3 = Add (Card (Numeric 5) Hearts) (Add (Card (Numeric 9) Hearts) empty)
 hand4 = Add (Card (Numeric 5) Hearts) (Add (Card (Numeric 9) Hearts) (Add (Card (Numeric 10) Hearts) empty))
 
+-- empty function
 empty :: Hand
 empty = Empty
 
+-- valueRank assigning values to the rank of the card. No implementation for aces so far
 valueRank :: Rank -> Integer
 valueRank Jack = 10
 valueRank Queen = 10
@@ -21,17 +23,19 @@ valueRank King = 10
 valueRank Ace = 11
 valueRank (Numeric i) = i
 
+-- valueCard relies on valueRank to give values to each card
 valueCard :: Card -> Integer
 valueCard card = valueRank (rank card)
---valueCard _ = 0
---valueCard a = valueRank (rank a)
 
+-- if the hand is empty the value is 0 otherwise it itterates through all cards in the hand and adds their value to the value of the hand
 value :: Hand -> Integer
 value Empty = 0
 value (Add card hand) = valueCard card + value hand
 
+-- if the hand is over 21 the game is over. Relies on value function.
 gameOver :: Hand -> Bool
 gameOver hand = value hand > 21
 
+-- winner decided with help from value if the bank or the guest has won.
 winner :: Hand -> Hand -> Player
 winner guest bank = if((value guest) > (value bank)) then Guest else Bank
