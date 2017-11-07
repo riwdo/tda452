89f75be59e882a8cafe6b739bf5f4418ba168692
oscar hansson <racso.hansson@gmail.com>
2017-11-07 11:28:58 +0100

    So good

--- blackjack/BlackJack.hs
+++ blackjack/BlackJack.hs
@@ -5,13 +5,11 @@ import RunGame
 import Test.QuickCheck
 
 
-
 -- Implement functions for "empty", "value", "gameOver", and "winner"
 hand1 = Add (Card Jack Hearts) empty
 hand2 = Add (Card (Numeric 5) Hearts) (Add (Card (Numeric 9) Hearts) empty)
 hand3 = Add (Card Jack Hearts) empty
 hand4 = Add (Card Jack Hearts) empty
-
 
 empty :: Hand
 empty = Empty
@@ -23,11 +21,11 @@ valueRank King = 10
 valueRank Ace = 11
 valueRank (Numeric i) = i
 
-valueCard :: Hand -> Integer
-valueCard (Add card hand) = valueRank (rank card) + valueCard hand
-valueCard _ = 0
+valueCard :: Card -> Integer
+valueCard card = valueRank (rank card)
+--valueCard _ = 0
 --valueCard a = valueRank (rank a)
 
 value :: Hand -> Integer
 value Empty = 0
-value hand = valueCard hand
+value (Add card hand) = valueCard card + value hand
