4da3f88cee33cf0611c64235be3e91a88a64555c
Baloo <karlmattsson96@gmail.com>
2017-11-07 11:02:48 +0100

    Fixed values for hand

--- blackjack/BlackJack.hs
+++ blackjack/BlackJack.hs
@@ -7,6 +7,11 @@ import Test.QuickCheck
 
 
 -- Implement functions for "empty", "value", "gameOver", and "winner"
+hand1 = Add (Card Jack Hearts) empty
+hand2 = Add (Card (Numeric 5) Hearts) (Add (Card (Numeric 9) Hearts) empty)
+hand3 = Add (Card Jack Hearts) empty
+hand4 = Add (Card Jack Hearts) empty
+
 
 empty :: Hand
 empty = Empty
@@ -16,9 +21,13 @@ valueRank Jack = 10
 valueRank Queen = 10
 valueRank King = 10
 valueRank Ace = 11
-valueRank Numeric i = i
+valueRank (Numeric i) = i
 
-valueCard :: Card -> Integer
-
+valueCard :: Hand -> Integer
+valueCard (Add card hand) = valueRank (rank card) + valueCard hand
+valueCard _ = 0
+--valueCard a = valueRank (rank a)
 
 value :: Hand -> Integer
+value Empty = 0
+value hand = valueCard hand
