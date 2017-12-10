0947c1f848edba8811e08b743cf50a1f456946ca
oscar hansson <racso.hansson@gmail.com>
2017-12-10 12:58:33 +0100

    added blank function

--- 9 men's morris game/Morris.hs
+++ 9 men's morris game/Morris.hs
@@ -5,17 +5,22 @@ data HandMan = Empty | Add Man HandMan deriving (Show, Eq)
 
 data Man = Man deriving (Show, Eq)
 
+type Pos = (Int, Int)
+
 newtype Morris = Morris {rows :: [[Maybe Man]]}
   deriving (Show, Eq)
 
 startingMorris = Morris [[n,n,n]
                         ,[n,n,n]
                         ,[n,n,n]
-                        ,[n,n,n,n,n,n]
+                        ,[Just m,n,Just m,n,n,n]
                         ,[n,n,n]
                         ,[n,n,n]
                         ,[n,n,n]]
                         where n = Nothing
                               m = Man
 
-                        
+blanks :: Morris -> [(Int,Int)]
+blanks (Morris board) = [(x,col) | (x,row) <- zip [0..8] board
+                          , col <- blanks' row]
+                          where blanks' row  = [col | (col,value) <- zip [0..8] row, isNothing value]
